0099ca10d7dafbf9f092f0701558fe3f56e495b0
Matheus Leandro <mattleandroo@outlook.com>
2019-09-26 15:26:54 -0300

    change style

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
-var listElement = document.querySelector('ul');
 var inputElement = document.querySelector('input');
+var listElement = document.querySelector('ul');
 
 function renderRepositories(repositories) {
-    listElement.innerHTML = "";
+	listElement.innerHTML = "";
 
-    for (repo of repositories) {
-        const textElement = document.createTextNode(repo.name);
-        const liElement = document.createElement('li');
+	for (repo of repositories) {
+		const textElement = document.createTextNode(repo.name);
+		const liElement = document.createElement('li');
 
-        liElement.appendChild(textElement);
-        listElement.appendChild(liElement);
-    }
+		liElement.appendChild(textElement);
+		listElement.appendChild(liElement);
+	}
 }
 
 function renderLoading(loading) {
-    listElement.innerHTML = "";
+	listElement.innerHTML = "";
 
-    var textElement = document.createTextNode('Carregando...');
-    var loadingElement = document.createElement('li');
+	var textElement = document.createTextNode('Loading...');
+	var loadingElement = document.createElement('li');
 
-    loadingElement.appendChild(textElement);
-    listElement.appendChild(loadingElement);
+	loadingElement.appendChild(textElement);
+	listElement.appendChild(loadingElement);
 }
 
 function renderError(loading) {
-    listElement.innerHTML = "";
+	listElement.innerHTML = "";
 
-    var textElement = document.createTextNode('Erro!');
-    var errorElement = document.createElement('li');
+	var textElement = document.createTextNode('Erro during the process or repositorie not found! Try again.');
+	var errorElement = document.createElement('li');
 
-    errorElement.style.color = "#F00";
-    errorElement.appendChild(textElement);
-    listElement.appendChild(errorElement);
+	errorElement.style.color = "#F00";
+	errorElement.appendChild(textElement);
+	listElement.appendChild(errorElement);
 }
 
 function listRepositories() {
-    var user = inputElement.value;
+	var user = inputElement.value;
 
-    if (!user) return;
+	if (!user) return;
 
-    renderLoading();
+	renderLoading();
 
-    axios.get('https://api.github.com/users/' + user + '/repos')
-        .then(function(response) {
-            renderRepositories(response.data);
-        })
-        .catch(function() {
-            renderError();
-        });
+	axios.get('https://api.github.com/users/' + user + '/repos')
+		.then(function (response) {
+			renderRepositories(response.data);
+		})
+		.catch(function () {
+			renderError();
+		});
 }
